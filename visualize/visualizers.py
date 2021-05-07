d3ce5c2c9a2a7d8955e0405679884800457daa3c
Prathamesh Kulkarni <prathameshk2696@gmail.com>
2021-05-07 13:58:16 -0400

    Updated visualize

--- visualize/visualizers.py
+++ visualize/visualizers.py
@@ -1,15 +1,15 @@
 
 from matplotlib import pyplot as plt
 
-def show_l_inf_plot(kind='',dim='',method='',spectral_radius='',l_inf_values=[],s=1):
-    plt.scatter(x = range(0,len(l_inf_values)),y = l_inf_values,s=s)
+def show_l_inf_plot(kind='',dim='',method='',spectral_radius='',l_inf_values=[],s=10):
+    plt.scatter(x = range(0,len(l_inf_values)),y = l_inf_values,s=s,c='#0303fc')
     plt.title(f'Kind : {kind}\nDimension : {dim}\nMethod : {method}\nSpectral radius : {spectral_radius}')
     plt.xlabel('Iteration Number')
-    plt.ylabel('L_inf',rotation=0)
+    plt.ylabel('||X-X*||_inf')
     plt.show()
 
-def show_iterations_plot(kind='',dim='',y=[],iteration_values=[],s=1):
-    plt.barh(y=y[::-1],width=iteration_values[::-1])
+def show_iterations_plot(kind='',dim='',y=[],iteration_values=[],s=1,color='blue'):
+    plt.barh(y=y[::-1],width=iteration_values[::-1],color=color)
     plt.xlim([0,max(iteration_values)*1.1])
     for index,value in enumerate(iteration_values[::-1]):
         plt.text(value,index,str(value))
@@ -18,8 +18,8 @@ def show_iterations_plot(kind='',dim='',y=[],iteration_values=[],s=1):
     plt.ylabel('Method')
     plt.show()
     
-def show_spectral_radius_plot(kind='',dim='',y=[],spectral_radius_values=[],s=1):
-    plt.barh(y=y[::-1],width=spectral_radius_values[::-1])
+def show_spectral_radius_plot(kind='',dim='',y=[],spectral_radius_values=[],s=1,color='blue'):
+    plt.barh(y=y[::-1],width=spectral_radius_values[::-1],color=color)
     plt.xlim([0,max(spectral_radius_values)+1])
     for index,value in enumerate(spectral_radius_values[::-1]):
         plt.text(value,index,str(value))
