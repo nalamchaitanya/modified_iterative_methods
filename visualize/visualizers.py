4596430091b40c37c6bf96b9fccfe360cb2c7b0b
Sai Krishna Chaitanya Nalam Venkata Subrahmanya <nalakris@adobe.com>
2021-05-08 15:16:00 -0400

    Minor aesthetic changes in visualization

--- visualize/visualizers.py
+++ visualize/visualizers.py
@@ -10,9 +10,9 @@ def show_l_inf_plot(kind='',dim='',method='',spectral_radius='',l_inf_values=[],
 
 def show_iterations_plot(kind='',dim='',y=[],iteration_values=[],s=1,color='blue'):
     plt.barh(y=y[::-1],width=iteration_values[::-1],color=color)
-    plt.xlim([0,max(iteration_values)*1.1])
+    plt.xlim([min(iteration_values)*0.9,max(iteration_values)*1.1])
     for index,value in enumerate(iteration_values[::-1]):
-        plt.text(value,index,str(value))
+        plt.text(value,index,str(round(value,6)))
     plt.title(f'Kind : {kind}\nDimension : {dim}\n')
     plt.xlabel('Number of Iterations')
     plt.ylabel('Method')
@@ -20,9 +20,10 @@ def show_iterations_plot(kind='',dim='',y=[],iteration_values=[],s=1,color='blue
     
 def show_spectral_radius_plot(kind='',dim='',y=[],spectral_radius_values=[],s=1,color='blue'):
     plt.barh(y=y[::-1],width=spectral_radius_values[::-1],color=color)
-    plt.xlim([0,max(spectral_radius_values)+1])
+    width = max(spectral_radius_values) - min(spectral_radius_values)
+    plt.xlim([min(spectral_radius_values)-width/2,max(spectral_radius_values)+width/2])
     for index,value in enumerate(spectral_radius_values[::-1]):
-        plt.text(value,index,str(value))
+        plt.text(value,index,str(round(value,6)))
     plt.title(f'Kind : {kind}\nDimension : {dim}\n')
     plt.ylabel('Method')
     plt.xlabel('Spectral Radius')
